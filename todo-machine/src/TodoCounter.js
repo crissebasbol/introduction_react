5f6afaaf49b3767e2af520551201ae537a89460e
Cristhian Sebastian Bolaños Portilla <cbolanosportilla@twilio.com>
2022-12-10 21:16:23 -0500

    example styles

--- todo-machine/src/TodoCounter.js
+++ todo-machine/src/TodoCounter.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+const styles = {
+  color: "red",
+  backgroundColor: "yellow",
+};
+
 function TodoCounter() {
-  return <h2>You completed 2 of 3 TODOs</h2>;
+  return <h2 style={styles}>You completed 2 of 3 TODOs</h2>;
 }
 
 export { TodoCounter };
