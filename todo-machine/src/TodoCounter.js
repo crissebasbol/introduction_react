53e10f78523d3abb2b4948814ee811fa2ce0bcd0
Cristhian Sebastian Bolaños Portilla <cbolanosportilla@twilio.com>
2022-12-10 21:20:54 -0500

    Change form apply styles with css

--- todo-machine/src/TodoCounter.js
+++ todo-machine/src/TodoCounter.js
@@ -1,12 +1,8 @@
 import React from "react";
-
-const styles = {
-  color: "red",
-  backgroundColor: "yellow",
-};
+import "./TodoCounter.css";
 
 function TodoCounter() {
-  return <h2 style={styles}>You completed 2 of 3 TODOs</h2>;
+  return <h2 className="TodoCounter">You completed 2 of 3 TODOs</h2>;
 }
 
 export { TodoCounter };
