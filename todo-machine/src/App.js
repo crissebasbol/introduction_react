5a7fefcaf3e119a18ce1087a1bb8faf8916eab80
Cristhian Sebastian Bolaños Portilla <cbolanosportilla@twilio.com>
2022-12-10 21:08:32 -0500

    Creation TODO components

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -1,24 +1,41 @@
-import './App.css';
-import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoCounter } from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { TodoList } from "./TodoList";
+import { TodoSearch } from "./TodoSearch";
+
+const todo_list = [
+  {
+    text: "Buy onions",
+    completed: false,
+  },
+  {
+    text: "Finish course React",
+    completed: false,
+  },
+  {
+    text: "Make design",
+    completed: false,
+  },
+];
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.children}
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todo_list.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
